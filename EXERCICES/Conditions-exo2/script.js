8b9271606a05d299752ac9cda45428f0ef0a4611
hussam <elmoudenhoussam866@gmail.com>
2025-09-19 11:34:14 +0100

    Add files via upload

---
+++ EXERCICES/Conditions-exo2/script.js
@@ -0,0 +1,14 @@
+let note= prompt("enter your note :")
+
+if (note < 10)
+{
+    alert ("Insuffisant");
+}
+else if (note >= 10 && note <= 12)
+{
+    alert ("Passable");
+}
+else
+{
+    alert ("Bien");
+}
