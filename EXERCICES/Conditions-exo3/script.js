53edbc7f057d1043821481b06f2f97d512872b82
hussam <elmoudenhoussam866@gmail.com>
2025-09-19 11:34:41 +0100

    Add files via upload

---
+++ EXERCICES/Conditions-exo3/script.js
@@ -0,0 +1,34 @@
+let days= prompt("entrer un nombre entre 1 et 7 :")
+
+if (days == 1)
+{
+    alert("Lundi");
+}
+else if (days == 2)
+{
+    alert("Mardi");
+}
+else if (days == 3)
+{
+    alert("Mercredi");
+}
+else if (days == 4)
+{
+    alert("Jeudi");
+}
+else if (days == 5)
+{
+    alert("Vendredi");
+}
+else if (days == 6)
+{
+    alert("Samedi");
+}
+else if (days == 7)
+{
+    alert("Dimanche");
+}
+else 
+{
+    alert("Numero invalide");
+}
